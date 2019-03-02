a20b615141bb7e2398cfe0724afffa104ca88441
Romain Jourde <dust63@hotmail.fr>
2019-03-02 21:22:38 +0100

    Migrate .netstandard and 4.6.1 for exmaple project

---
+++ Examples/ReadFromFile/Example.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+using Rug.Osc;
+
+namespace ReadFromFile
+{
+	public partial class Example : Form
+	{
+		public Example()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Append a line to the output box
+		/// </summary>
+		/// <param name="line">the line to append</param>
+		void AppendLine(string line)
+		{
+			m_Output.AppendText(line + Environment.NewLine);
+			m_Output.Select(m_Output.TextLength, 0);
+			m_Output.ScrollToCaret();
+		}
+
+		private void Example_Load(object sender, EventArgs e)
+		{
+			m_Format.Items.Clear();
+			m_Format.Items.AddRange(Enum.GetNames(typeof(OscPacketFormat)));
+			m_Format.SelectedIndex = 0;
+
+			m_OpenFileDialog.Title = "Open osc packet file";
+			m_OpenFileDialog.Filter = "All files (*.*)|*.*"; 
+		}
+
+		private void SelectFile_Click(object sender, EventArgs e)
+		{
+			m_OpenFileDialog.ShowDialog(); 
+		}
+
+		private void OpenFileDialog_FileOk(object sender, CancelEventArgs e)
+		{
+			m_FilePath.Text = m_OpenFileDialog.FileName; 
+		}
+
+		private void Format_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Go_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				using (FileStream stream = new FileStream(m_FilePath.Text, FileMode.Open, FileAccess.Read))
+				using (OscReader reader = new OscReader(stream, (OscPacketFormat)m_Format.SelectedIndex))
+				{
+					while (reader.EndOfStream == false) 
+					{
+						AppendLine(reader.Read().ToString()); 
+					}
+
+					AppendLine("End of file"); 
+				}				
+			}
+			catch (Exception ex)
+			{
+				AppendLine("Exception while reading");
+				AppendLine(ex.Message);
+			}
+		}
+
+		private void Clear_Click(object sender, EventArgs e)
+		{
+			m_Output.Clear(); 
+		}
+	}
+}
---
+++ Rug.Osc.Tests/Rug.Osc.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Rug.Osc.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rug.Osc.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0400d0ca-f825-4bfe-aa51-69990ac66cc1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
